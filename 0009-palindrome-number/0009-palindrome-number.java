38fc840bbb0a68a7c522f38547aafabe223a9889
Nishant Gaurav <118832071+Imbiber@users.noreply.github.com>
2023-10-02 22:16:05 +0530

    Time: 5 ms (71.86%), Space: 42.7 MB (65.77%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,32 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x); // Convert to String
-        int n = s.length(); // Store the String length to int n
-
-        for (int i=0; i<n/2; i++) {
-            // We check whether the elements at the same distance from
-            // beginning and from ending are same, if not we return false
-            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+        if(x<0 || x!=0 && x%10 ==0 ) return false;
+        int check=0;
+        while(x>check){
+            check = check*10 + x%10;
+            x/=10;
         }
-
-        // if no flaws are found we return true
-        return true;
+        return (x==check || x==check/10);
     }
 }
-
-//below code doesnt work for negative number so find the reason
-// //count the digits for length of array
-//         int numDigits = (int) Math.log10(x) + 1;
-
-//         int[] digits = new int[numDigits];
-//         for (int i = numDigits - 1; i >= 0; i--) {
-//         digits[i] = x % 10;
-//         x /= 10;
-//         }
-//         int n=digits.length;
-//         for(int i=0;i<n;i++){
-//             if (digits[i]!=digits[(n-1-i)]){
-//                 return false;
-//             }           
-//         }
-//         return true;
